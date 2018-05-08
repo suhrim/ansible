a5046511e56f682271849bce750b88ce84f478f9
tobeve <tobias.evert@nordnet.se>
2018-05-08 13:14:19 +0200

    Treat blank policy strings as empty ones

--- lib/ansible/modules/messaging/rabbitmq_policy.py
+++ lib/ansible/modules/messaging/rabbitmq_policy.py
@@ -106,7 +106,7 @@ class RabbitMqPolicy(object):
         policies = self._exec(['list_policies'], True)
 
         for policy in policies:
-            if not policy:
+            if not policy or policy.isSpace():
                 continue
             policy_name = policy.split('\t')[1]
             if policy_name == self._name:
